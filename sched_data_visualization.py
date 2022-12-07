f9a1f735fff9c7d1e56f9db8a9d5ca7054e27bd5
Petder02 <pjs182@case.edu>
2022-12-07 16:47:36 -0500

    Created function for retrieving pid sched maps
    Made sure to exclude any pids that do not actually have a sched file available to read

--- sched_data_visualization.py
+++ sched_data_visualization.py
@@ -3,10 +3,12 @@ import os
 """ 
 Finds all the active processes on the system
 
-Input: N/A
-Output: A list containing the PIDs of all processes (non-active and active) on the system
+Input: 
+    N/A
+Output: 
+    A list containing the PIDs of all processes (non-active and active) on the system
 """
-def find_all_pids() -> list:
+def find_all_pids() -> list[str]:
     # Run top, but without the headers. Only want the PID's from the overall output
     processes = os.popen("top -b -n 1 | sed -n '/PID/,$p'|sed '/PID/d'")
     top_output = processes.read()
@@ -24,17 +26,36 @@ def find_all_pids() -> list:
             for line in file_out:
                 line = line.strip()
                 segments = line.split()
-                print(line)
-                print(segments)
                 # If there are no values, return an error cause this should never happen
                 if len(segments) <= 0:
-                    raise ValueError("No data for this PID (should never occur)!")
+                    raise Exception("No data for this PID (should never occur)!")
                 elif not segments[0].isnumeric():
-                    raise ValueError("The first data input should be a PID!")
+                    raise Exception("The first data input should be a PID!")
                 else:
                     pids.append(segments[0])
         return pids
-    except ValueError:
-        print ("Something went wrong with reading from top_results!")
-    
-print(find_all_pids())
+    except Exception as e:
+        print(e)
+
+"""
+Gets all the sched map paths for the pids found in top
+
+Input:
+    pids -> All the pids found in top on this run
+Output:
+    A list with all the sched paths for the pids found after reading from top
+"""
+def find_pid_sched_paths(pids: list[str]) -> list[str]:
+    # Change working directory to proc
+    os.chdir("/proc")
+    sched_paths = []
+    for pid in pids:
+        try:
+            sched_path = f"/proc/{pid}/sched"
+            # Exclude any processes which do not have sched files
+            if not os.path.exists(sched_path):
+                raise Exception(f"The sched directory with path -> {sched_path} does not exist!\nIt will be excluded from the final analysis.")
+            sched_paths.append(f"/proc/{pid}/sched")
+        except Exception as e:
+            print(e)
+    return sched_paths
