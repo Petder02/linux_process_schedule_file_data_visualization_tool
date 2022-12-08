f6395fbaf9c0ef1200fdbbc68de3bb8f9b3ec4d0
Petder02 <pjs182@case.edu>
2022-12-08 16:41:44 -0500

    Added a few things
    1. Short and full options -> Short prints abbreviated column names and the results to terminal along with writing them to a file, long prints full column names and only writes the output to a file due to the size of the dataframe. Modified output dataframe column names in the case of the 'short' option
    
    2.) Added a main function used to run the script
    
    3.) Made it so the program runs until it is killed by the user (ctrl+c) so they can get as many snapshots of sched information for a set number of processes as possible
    
    4.) Added a num_pids option (must be > 0) used to specify the number of pids' sched data you want to print to a table. This can be any positive number ,but if the input number exceeds the number of pids available, then all available pids will just be read instead.
    
    Most of what remains is just cleaning up the code and existing comments

--- sched_data_visualization.py
+++ sched_data_visualization.py
@@ -1,9 +1,11 @@
+# System level imports
 import os
+import sys
+from time import sleep
+
+# Data visualization libraries used
 import pandas as pd
 import numpy as np
-
-# importing the modules
-from IPython.display import HTML
 from tabulate import tabulate
 
 """ 
@@ -14,7 +16,7 @@ Input:
 Output: 
     A list containing the PIDs of all processes (non-active and active) on the system
 """
-def find_all_pids(): #-> list[str]:
+def find_all_pids():
     # Run top, but without the headers. Only want the PID's from the overall output
     processes = os.popen("top -b -n 1 | sed -n '/PID/,$p'|sed '/PID/d'")
     top_output = processes.read()
@@ -52,7 +54,7 @@ Input:
 Output:
     A dictionary mapping each pid to its sched file (if the sched file exists)
 """
-def find_pid_sched_paths(pids):#: list[str]) -> dict[int, str]:
+def find_pid_sched_paths(pids):
     # Change working directory to proc
     # os.chdir("/proc")
     sched_paths = dict()
@@ -76,7 +78,7 @@ Input:
 Output:
     A dict associating pids with information from their sched file
 """
-def construct_pid_sched_file_info_dict(pid_to_sched_file):#: dict[int, str]) -> dict[int, list[str]]:
+def construct_pid_sched_file_info_dict(pid_to_sched_file):
     pid_sched_file_info_dict = {}
     line_break_char = '-'
     # Iterate through sched files, get the necessary data
@@ -121,56 +123,140 @@ def construct_pid_sched_file_info_dict(pid_to_sched_file):#: dict[int, str]) -> 
     return pid_sched_file_info_dict
 
 """
-Constructs data frame containing information found in sched
+Constructs the sched data frame based on the given sched info for each pid
 
 Input:
-    pid_sched_file_info_dict -> A dict containing sched info per process
+    pid_sched_file_info_dict -> A dictionary mapping each pid to its sched data
+    format_option            -> A string specifying which column names to use for the data frame
+
 Output:
-    A data frame containing sched information organized by process, across all processes
+    A data frame contatining all information from sched for each pid
+    
 """
-def construct_sched_data_frame(pid_sched_file_info_dict):#: dict[str]):
-    # Create data frame with structure
-    sched_data = {
-        "se.exec_start": [],
-        "se.vruntime": [],
-        "se.sum_exec_runtime": [],
-        "se.nr_migrations": [],
-        "nr_switches": [],
-        "nr_voluntary_switches": [],
-        "nr_involuntary_switches": [],
-        "se.load.weight": [],
-        "se.avg.load_sum": [],
-        "se.avg.runnable_sum": [],
-        "se.avg.util_sum": [],
-        "se.avg.load_avg": [],
-        "se.avg.runnable_avg": [],
-        "se.avg.util_avg": [],
-        "se.avg.last_update_time": [],
-        "se.avg.util_est.ewma": [],
-        "se.avg.util_est.enqueued": [],
-        "policy": [],
-        "prio": [],
-        "clock-delta": []
-    }
+def construct_sched_data_frame(pid_sched_file_info_dict, format_option):
+    # Create data frame with structure depending on input option
+    sched_data = {}
+    if format_option == "long":
+        sched_data = {
+            "PID": [],
+            "se.exec_start": [],
+            "se.vruntime": [],
+            "se.sum_exec_runtime": [],
+            "se.nr_migrations": [],
+            "nr_switches": [],
+            "nr_voluntary_switches": [],
+            "nr_involuntary_switches": [],
+            "se.load.weight": [],
+            "se.avg.load_sum": [],
+            "se.avg.runnable_sum": [],
+            "se.avg.util_sum": [],
+            "se.avg.load_avg": [],
+            "se.avg.runnable_avg": [],
+            "se.avg.util_avg": [],
+            "se.avg.last_update_time": [],
+            "se.avg.util_est.ewma": [],
+            "se.avg.util_est.enqueued": [],
+            "policy": [],
+            "prio": [],
+            "clock-delta": []
+        }
+    else:
+        sched_data = {
+            "PID": [],
+            "EXEC_ST": [],
+            "VRT": [],
+            "SERT": [],
+            "NR_MIGRs": [],
+            "NR_SWs": [],
+            "NR_VOL_SWs": [],
+            "NR_INVOL_SWs": [],
+            "LD_WT": [],
+            "LD_SUM": [],
+            "RUNNABLE_SUM": [],
+            "UTIL_SUM": [],
+            "LD_AVG": [],
+            "RUNNABLE_AVG": [],
+            "UTIL_AVG": [],
+            "LAST_UPDT_TIME": [],
+            "EWMA": [],
+            "ENQUEUED": [],
+            "POLICY": [],
+            "PRIO": [],
+            "CLK_DETLA": []
+        }
     # Index dataframe by PID
     sched_data_frame = pd.DataFrame(sched_data)
     # Construct the dataframe row by row
     for pid in pid_sched_file_info_dict:
         sched_file_info = pid_sched_file_info_dict[pid]
+        # Make sure to insert pid at front for output
+        sched_file_info.insert(0, pid)
         # Add the row to the dataframe
         sched_data_frame.loc[len(sched_data_frame.index)] = sched_file_info
         
     return sched_data_frame
 
-pids = find_all_pids()
-pid_sched_paths = find_pid_sched_paths(pids)
-pid_sched_file_info_dict = construct_pid_sched_file_info_dict(pid_sched_paths)
-sched_data_frame = construct_sched_data_frame(pid_sched_file_info_dict)
-# Finally convert the sched_data to an html table
-# Given how much data there is, this should be helpful in the long run
-print(tabulate(sched_data_frame, headers='keys', tablefmt='psql'))
-html = sched_data_frame.to_html()
-# Write html output to a file
-text_file = open("index.html", "w")
-text_file.write(html)
-text_file.close()
+# pids = find_all_pids()
+# pid_sched_paths = find_pid_sched_paths(pids)
+# pid_sched_file_info_dict = construct_pid_sched_file_info_dict(pid_sched_paths)
+# sched_data_frame = construct_sched_data_frame(pid_sched_file_info_dict)
+# print(sched_data_frame)
+# print(tabulate(sched_data_frame, headers='keys', tablefmt='psql', showindex=False))
+
+# # Make output into a nice table format and write to a file for viewing
+# with open("sched_data.txt", 'w') as file:
+#     file.write(tabulate(sched_data_frame, headers='keys', tablefmt='psql', showindex=False))
+
+"""
+Prints the top n PIDs with their associated sched information in a table format for ease of visualization
+    
+Inputs taken from the command line arguments:
+    num_pids      -> An integer specifying the number of pids to print information for
+    format_option -> A string specifying the format which column names should be displayed in. There are two possible formats...
+        'short' -> Displays abbreviated sched data names
+        'long'  -> Displays full sched data names
+Output:
+    Using the 'short' format, the output will be generated directly on the command line
+    Using the 'long' format, the output will be generated to a text file called 'sched_output.txt'
+"""
+def main():
+    print(sys.argv)
+    # Arguments must be of specified types
+    num_pids = int(sys.argv[1])
+    format_option = str(sys.argv[2])
+    # Argument validation
+    if num_pids <= 0:
+        print("Must display the sched information for at least one process!")
+        return
+    if not (format_option == "short" or format_option == "long"):
+        print("Must supply a format option of either 'short' or 'long'!")
+        return
+    # Get sched info per pid available
+    pids = find_all_pids()
+    if num_pids > len(pids):
+        print(f"The specified number of pids exceeds the number of pids available.\nThe total number of pids available is {len(pids)}.")
+        num_pids = len(pids)
+    # Create sched_data given the pids
+    pid_sched_paths = find_pid_sched_paths(pids)
+    pid_sched_info_dict = construct_pid_sched_file_info_dict(pid_sched_paths)
+    sched_data = construct_sched_data_frame(pid_sched_info_dict, format_option)
+    # For the short version, also print the results to the console
+    # For the long version, because it will be incorrectly formatted in the console, only write it to a file
+    # The results of each iteration of the script will be recorded within that file for easy viewing
+    if format_option == "short":
+        print(f"{tabulate(sched_data.head(num_pids), headers='keys', tablefmt='psql', showindex=False)}\n\n")
+    print("Writing results to 'pid_sched_data.txt' in working directory.")
+    with open("pid_sched_data.txt", 'a') as file:
+        file.write(f"{tabulate(sched_data.head(num_pids), headers='keys', tablefmt='psql', showindex=False)}\n\n")
+            
+# Runs the program using the main function a set number of times
+# You can terminate the program by using CTRL+C, but otherwise it will iterate keep iterating
+if __name__ == '__main__':
+    # Create new pid_sched_data file
+    file = open("pid_sched_data.txt", 'w')
+    file.close()
+    while (True):
+        main()
+        # Delete files created by functions
+        
+        sleep(2.0)
